39d79628b180d41f486f603ad7f84d39576af1a5
Tornike Kapanadze <tornikekapan@gmail.com>
2025-11-28 14:23:26 +0200

    fix: format code

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ export default {
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest'
   },
-  transformIgnorePatterns: ['node_modules/(?!(bare-crypto|expo-asset|pear-apps-utils-validator)/)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(bare-crypto|expo-asset|pear-apps-utils-validator)/)'
+  ],
   setupFiles: ['<rootDir>/jest.setup.js']
 }
